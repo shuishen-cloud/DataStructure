6ded94fa5c84e255149b9e554b48807c3b0e3c22
Wsl_Arch_lwy <201212ss@sina.com>
2025-08-16 15:36:42 +0800

    构成基本项目和 CMake 项目

---
+++ README.md
@@ -0,0 +1,6 @@
+这个项目似乎还是又不少不稳定之处。
+
+
+### 注意
+
+`Wsl` 下 `Arch` (*Wsl 的 Ubuntu 下可以直接使用*)的 `Vscode` 可能检测不到语言服务器 `clangd` 的目录，需要手动在 `.vscode/settings` 中添加路径。
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Filename:  main.cpp
+ * Project:   src
+ * Author:    lwy
+ * ***
+ * Created:   2025/08/16 Saturday 15:40:49
+ * Modified:  2025/08/16 Saturday 16:05:44
+ * ***
+ * Description: 测试 CMake 项目构建运行
+ */
+
+#include <iostream>
+
+int main(void){
+    
+    std::cout << "Hello from CMakeProject!" << std::endl;
+
+    return 0;
+}
